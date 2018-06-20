236b8662a9f0dd9e728e5a24fd9783d7d54d0d56
timrooke1991 <timrooke1991@gmail.com>
2018-06-20 13:08:47 +0100

    Cleaned up routing of the app

--- config/routes.js
+++ config/routes.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
 const urlController = require('../controllers/urls');
 
+router.route('/:alias')
+  .get(urlController.show);
+
 router.route('/')
   .get(urlController.index)
   .post(urlController.create);
 
-router.route('/r/:alias')
-  .get(urlController.show);
-
-// router.all('*', (req, res) => res.notFound());
+router.all('*', (req, res) => res.notFound());
 
 module.exports = router;
